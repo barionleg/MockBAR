8af7e3394fa89e586061f403d03c4b231dd76f20
Bankn8II©$A <102619282+barionleg@users.noreply.github.com>
2022-06-19 07:32:58 +0200

    Create gtag.js

---
+++ gtag.js
@@ -0,0 +1,9 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-4C7D3LVHJ7"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-4C7D3LVHJ7');
+</script>
